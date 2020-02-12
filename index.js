d124065bd3e306e8bc01d71d6737cd3f38935790
marcusline <46751773+marcusline@users.noreply.github.com>
2020-02-12 12:18:22 +0100

    Update 1

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const fs =require('fs');
+const app=express();
+app.set('view engine', 'pug');
+
+
+let rawdata = fs.readFileSync('Chassi.json');
+let chassi = JSON.parse(rawdata);
+console.log(chassi);
+
+
+app.get('/', function (req,res){
+    res.render('index', {rubrik: JSON.stringify(chassi)});
+})
+
+
+app.listen(3000);
+
+
+

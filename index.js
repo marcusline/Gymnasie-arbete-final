ee5db544fec294de348f0986bc564ffd6a667567
marcusline <46751773+marcusline@users.noreply.github.com>
2020-03-06 14:15:17 +0100

    ppog

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const fs =require('fs');
 const app=express();
+const path=require('path');
 app.set('view engine', 'ejs');
 
 
@@ -10,7 +11,11 @@ console.log(chassi);
 
 
 app.get('/', function (req,res){
-    res.render('index', {rubrik: "Bootstrap text", text: JSON.stringify(chassi[0], 3),});
+    //res.render('index', {rubrik: "Bootstrap text", text: JSON.stringify(chassi[0], 3),});
+    res.render(path.join(__dirname, 'views/index.ejs'),{
+        text: JSON.stringify(chassi[0], 3),
+        rubrik: "Bootstrap text"
+    });
 })
 
 
